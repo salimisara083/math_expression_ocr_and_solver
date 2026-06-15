21c95dba293bcbc28d71f1dc0250e152982a8329
Sara Salimi <salimisara083@gmail.com>
2026-06-16 00:47:28 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -81,7 +81,7 @@ class CustomTrainLoader:
 
         self.loader = DataLoader(
             self.dataset,
-            batch_size=128,
+            batch_size=200,
             shuffle=True,
             collate_fn=collate,
             num_workers=4,
