0046d9b35c96efb0cb4288e5d996376e8fecfd19
Sara Salimi <salimisara083@gmail.com>
2026-06-16 11:27:42 +0330

    Refactor CustomTrainLoader and adjust batch size
    Updated batch size in DataLoader and improved code formatting.

--- utils.py
+++ utils.py
@@ -81,7 +81,7 @@ class CustomTrainLoader:
 
         self.loader = DataLoader(
             self.dataset,
-            batch_size=256,
+            batch_size=240,
             shuffle=True,
             collate_fn=collate,
             num_workers=4,
